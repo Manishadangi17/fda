7d866a4cf03e9e03798345d3f98a62f0b77bb7cf
Manishadangi17 <manisha.dangi@capgemini.com>
2023-06-13 16:57:27 +0530

    removed duplicate project

--- FoodDeliveryAppOrder/src/main/java/com/capg/FoodDeliveryAppOrder/repository/copy/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capg.FoodDeliveryAppOrder.repository.copy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capg.FoodDeliveryAppOrder.entity.Order;
-
-public interface OrderRepository  extends JpaRepository<Order, Integer> {
-
-}
